8fc32c035e4182e570f36ba591d88914ef8468e4
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-09 10:59:51 +0200

    andet cmd commit

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,19 @@
+const mysql = require('mysql2');
+
+const db2 = mysql.createConnection({
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'testdatabase2'
+});
+
+const db1 = mysql.createConnection({
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'testdatabase1'
+});
+
 module.exports = function(app){
 
 	app.get('/', function(req, res){
@@ -20,4 +36,23 @@ module.exports = function(app){
 		}
 		res.send(home);
 	});
+
+	app.get('/getAllUsers', function (req, res) {
+		db1.query('SELECT fornavn FROM bruger', function (err, rows) {
+			if (err) {
+				res.send(err)
+			} else {
+				res.send(rows)
+			}
+		})
+	})
+	app.get('/getAllProducts', function (req, res) {
+		db2.query('SELECT * FROM produkter', function (err, rows) {
+			if (err) {
+				res.send(err)
+			} else {
+				res.send(rows)
+			}
+		})
+	})
 }
